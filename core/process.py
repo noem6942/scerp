accf3a2b264215b7e873abf45358f845563b1345
noem6942 <42142505+noem6942@users.noreply.github.com>
2024-11-22 21:35:19 +0100

    update groups

--- core/process.py
+++ core/process.py
@@ -1,18 +1,80 @@
+# core/process.py
 from django.contrib.auth.models import Group, Permission
 import logging
 
 logger = logging.getLogger(__name__)  # Using the app name for logging
 
 USER_GROUPS = [
+    # Testing
+    {'name': 'local_user', 'permissions': []},
+    
+    # Funktionen
+    {'name': 'Funktion Zweckverband', 'permissions': []},
+    
+    # Gemeinderat    
+    {'name': 'Gemeinderat', 'permissions': []},
+    {'name': 'Gemeinderat - Präsidiales', 'permissions': []},
+    {'name': 'Gemeinderat - Finanzen', 'permissions': []},
+    {'name': 'Gemeinderat - Sicherheit', 'permissions': []},
+    {'name': 'Gemeinderat - Soziales', 'permissions': []},
+    {'name': 'Gemeinderat - Planung/Wirtschaft/Tourismus', 'permissions': []},
+    {'name': 'Gemeinderat - Hoch-/Tiefbau', 'permissions': []},
+    {'name': 'Gemeinderat - Bildung/Kultur', 'permissions': []},
+    
+    # Intern
+    {'name': 'Admin', 'permissions': []},
     {
-        'name': 'local_user',
+        'name': 'Intern Leitung',
         'permissions': [
-            'core.add_person',
-            'core.change_person',
-            'core.delete_person',
-            'core.view_person',
         ]
     },
+    {
+        'name': 'Intern Kanzlei',
+        'permissions': [
+        ]
+    },
+    {
+        'name': 'Intern Einwohnerdienste',
+        'permissions': [
+        ]
+    },
+    {
+        'name': 'Intern Personalwesen',
+        'permissions': [
+        ]
+    },
+    {
+        'name': 'Intern Gesundheit-/Sozialwesen',
+        'permissions': [
+        ]
+    },
+    {
+        'name': 'Intern Finanzen',
+        'permissions': [
+        ]
+    },
+    {
+        'name': 'Intern Steuern',
+        'permissions': [
+        ]
+    },
+    {
+        'name': 'Intern Liegenschaftsverwaltung',
+        'permissions': [
+        ]
+    },
+    {
+        'name': 'Intern Bauwesen',
+        'permissions': [
+        ]
+    },
+    {
+        'name': 'Intern Bildung',
+        'permissions': [
+        ]
+    },
+]
+'''    
     {
         'name': 'local_admin',
         'permissions': [
@@ -32,18 +94,18 @@ USER_GROUPS = [
             'auth.view_user',
         ]
     },
-]
+'''
 
 
 class UserGroup:
-    """
+    '''
     Manage user groups with specific permissions.
-    """
+    '''
 
     def update_or_create(self):
-        """
+        '''
         Create or update user groups with defined permissions.
-        """
+        '''
         for group_info in USER_GROUPS:
             # Create the group if it doesn't exist
             group, created = Group.objects.get_or_create(
@@ -51,17 +113,17 @@ class UserGroup:
             )
             if created:
                 logger.info(
-                    'Group "%s" created successfully.', group_info['name']
+                    f"Group '{ group_info['name']}' created successfully."
                 )
             else:
                 logger.warning(
-                    'Group "%s" already exists.', group_info['name']
+                    f"Group '{group_info['name']}' already exists."
                 )
 
             # Clear all existing permissions from the group
             group.permissions.clear()
             logger.info(
-                'All permissions removed from group "%s".', group_info['name']
+                f"All permissions removed from group `{group_info['name']}`."
             )
 
             # Assign new permissions to the group
@@ -75,13 +137,12 @@ class UserGroup:
                     )
                     group.permissions.add(permission)
                     logger.info(
-                        'Permission "%s" added to group "%s".',
-                        perm, group_info['name']
+                        f"Permission '{perm}' added to "
+                        f"group '{group_info['name']}'."                        
                     )
                 except Permission.DoesNotExist:
                     logger.error(
-                        'Permission "%s" does not exist.', perm
-                    )
+                        f"Permission '{perm}' does not exist.")
 
         logger.info('User group setup complete.')
         return True
