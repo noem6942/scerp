a2b52f41e0a74f3039ccd932ae2ac541402cccc6
noem6942 <42142505+noem6942@users.noreply.github.com>
2024-12-09 17:51:02 +0100

    introduce sphinx / docs

---
+++ core/_init_markdown.py
@@ -0,0 +1,30 @@
+# core/_init_markdown.py
+
+MODULE_PAGE = """<!-- source/{name}.md -->
+
+# {name}
+
+## Beschreibung
+{description}
+
+## Besonderheiten
+{features}
+- Easy to use
+- Written in Markdown
+- Integrated with Django
+
+## Daten
+{data}
+- Easy to use
+- Written in Markdown
+- Integrated with Django
+
+## Funktionen
+{functions}
+- Easy to use
+- Written in Markdown
+- Integrated with Django
+
+## Hinweise
+{notes}
+"""
